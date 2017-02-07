d0223f5f7e1f230be569e1027520c0e2c29c7448
JFreud <jrm.frdnbrg@gmail.com>
2017-02-06 19:28:07 -0500

    added hw01 - recursive sqrt

---
+++ 01HWRecursiveSqrt/Recursion.java
@@ -0,0 +1,38 @@
+public class Recursion {
+
+    public static String name() {
+	return "Freudenberg,Jerome";
+    }
+
+    public static double sqrt(double n) {
+	return findSqrt(n, 1.0);
+    }
+
+    public static double findSqrt(double n, double guess) {
+	if (n < 0) {
+	    throw new IllegalArgumentException();
+	}
+	if(isEqual(n, guess * guess)) {
+	    return guess;
+	}
+	 if(guess == 0.0) {
+	    return guess;
+	 }
+	double betterGuess = ((n / guess) + guess) / 2;
+	System.out.println(betterGuess);
+	return findSqrt(n, betterGuess);
+    }
+
+
+    public static boolean isEqual(double a, double b) {
+	if (Math.abs(a-b) < a/100000000000.0) {
+	    return true;
+	}
+	return false;
+    }
+
+    
+
+
+
+}
