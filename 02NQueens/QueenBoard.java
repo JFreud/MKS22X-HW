927bcbf1a0a9b220a288137d691fd2f3c9be61c0
JFreud <jrm.frdnbrg@gmail.com>
2017-02-09 18:01:39 -0500

    started nqueens

---
+++ 02NQueens/QueenBoard.java
@@ -0,0 +1,58 @@
+public class QueenBoard {
+    private int[][] board;
+    private int solutionCount;
+
+    QueenBoard (int size) {
+	board = new int[size][size];
+    }
+
+    private void addQueen(int r,int c) {
+	board[r][c] = -1;
+    }
+
+    private void removeQueen(int r, int c) {
+	if (board[r][c] == -1) {
+	    board[r][c] = 0;
+	}
+    }
+
+    private int countQueens() {
+	int total = 0;
+	for (int r = 0; r < board.length; r++) {
+	    for (int c = 0; c < board.length; c++) {
+		if (board[r][c] == -1) {
+		    total++;
+		}
+	    }
+	}
+	return total;
+    }
+
+	    
+
+    public boolean solve() {
+	return solveH(0, 0);
+    }
+
+
+    
+    public boolean solveHelper(row, col) {
+	if (countQueens() == board.length) {
+	    return true;
+	}
+	if (row == board.length) {
+	    return false;
+	}
+	if (board[r][c] == 0) {
+	    addQueen(r,c);
+	    for (int row = 0; row < board.length; row ++) {
+		if (board[row][c] == -1 && row != r) {
+		    removeQueen[r][c];
+		}
+		if (board [r][row] == -1 && row != c) {
+		    removeQueen[r][c];
+		}
+	    }
+	    
+    }
+}
