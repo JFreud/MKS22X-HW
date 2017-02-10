2b9db253b16453b3f10e0ff4f38c1b118bf3ae5a
JFreud <jrm.frdnbrg@gmail.com>
2017-02-10 12:24:51 -0500

    Wrote helper

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -31,28 +31,47 @@ public class QueenBoard {
 	    
 
     public boolean solve() {
-	return solveH(0, 0);
+	return solveHelper(0);
     }
 
 
     
-    public boolean solveHelper(row, col) {
+    public boolean solveHelper(int c) {
 	if (countQueens() == board.length) {
 	    return true;
 	}
 	if (row == board.length) {
 	    return false;
 	}
-	if (board[r][c] == 0) {
-	    addQueen(r,c);
-	    for (int row = 0; row < board.length; row ++) {
-		if (board[row][c] == -1 && row != r) {
+	for (int r = 0; r < board.length; r++) {
+
+	    if (board[r][c] == 0) {
+		addQueen(r,c);
+		for (int row = 0; row < board.length; row ++) {
+		    if (board[row][c] == -1 && row != r) {
+		    removeQueen[r][c];
+		    }
+		    if (board [r][row] == -1 && row != c) {
+		    removeQueen[r][c];
+		    }
+		}
+		checkDiagonals();
+		if (!Helper(col + 1)) {
 		    removeQueen[r][c];
 		}
-		if (board [r][row] == -1 && row != c) {
-		    removeQueen[r][c];
+		if (board[r][c] == -1){
+		    return Helper(col + 1);
 		}
 	    }
-	    
+	}
+	return false;
+    }
+
+    public int getSolutionCount(){
+    	return -1;
+    }
+
+    public String toString(){
+    	return "";
     }
 }
