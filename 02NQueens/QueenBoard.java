6d2fea376a7e3b3646233080fe5f253942546ee2
JFreud <jrm.frdnbrg@gmail.com>
2017-02-12 16:00:22 -0500

    count works but board isnt saved

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -1,86 +1,175 @@
 public class QueenBoard {
     private int[][] board;
+    private int[][] realBoard;
+    //private list solutions_list
     private int solutionCount;
+    private boolean works;
 
     QueenBoard (int size) {
 	board = new int[size][size];
+	works = false;
     }
 
     private void addQueen(int r,int c) {
 	board[r][c] = -1;
+	for (int col = c + 1 ; col < board.length; col++) {
+	    board[r][col] +=1;
+	}
+	for (int row = r + 1; row < board.length; row++) {
+	    board [row][c] +=1;
+	}
+	int rTL = r - 1, cTL = c - 1,
+	    rTR = r - 1, cTR = c + 1,
+	    rBL = r + 1, cBL = c - 1,
+	    rBR = r + 1, cBR = c + 1;
+
+	while (rTL >= 0 && cTL >= 0) {
+	    board[rTL][cTL] +=1;
+	    rTL -= 1;
+	    cTL -= 1;
+	}
+	while (rTR >= 0 && cTR < board.length) {
+	    board[rTR][cTR] += 1;
+	    rTR -=1;
+	    cTR +=1;
+	}
+	while (rBL < board.length && cBL >= 0) {
+	    board[rBL][cBL] += 1;
+	    rBL += 1;
+	    cBL -= 1;
+	}
+	while (rBR < board.length && cBR < board.length) {
+	    board[rBR][cBR] += 1;
+	    rBR += 1;
+	    cBR += 1;
+	}
+	
     }
 
     private void removeQueen(int r, int c) {
-	if (board[r][c] == -1) {
-	    board[r][c] = 0;
+	if (board[r][c] != -1) {
+	    System.out.println("No queen u dummy");
+	}
+        board[r][c] = 0;
+        for (int col = c + 1; col < board.length; col++) {
+	    board [r][col] -=1;
+	}
+	for (int row = r + 1; row < board.length; row++) {
+	    board [row][c] -=1;
+	}
+	
+	int rTL = r - 1, cTL = c - 1,
+	    rTR = r - 1, cTR = c + 1,
+	    rBL = r + 1, cBL = c - 1,
+	    rBR = r + 1, cBR = c + 1;
+	
+	while (rTL >= 0 && cTL >= 0) {
+	    board[rTL][cTL] -=1;
+	    rTL -= 1;
+	    cTL -= 1;
+	}
+	while (rTR >= 0 && cTR < board.length) {
+	    board[rTR][cTR] -= 1;
+	    rTR -=1;
+	    cTR +=1;
+	}
+	while (rBL < board.length && cBL >= 0) {
+	    board[rBL][cBL] -= 1;
+	    rBL += 1;
+	    cBL -= 1;
+	}
+	while (rBR < board.length && cBR < board.length) {
+	    board[rBR][cBR] -= 1;
+	    rBR += 1;
+	    cBR += 1;
 	}
     }
 
-    private int countQueens() {
-	int total = 0;
-	for (int r = 0; r < board.length; r++) {
-	    for (int c = 0; c < board.length; c++) {
-		if (board[r][c] == -1) {
-		    total++;
-		}
-	    }
-	}
-	return total;
-    }
-
-    // private void checkDiagonals(r, c) {
-    // 	int col = c;
-    // 	for (int row = r; row < board.length; row++) {
-    // 	    if (board[row][col] == -1) {
-    // 		removeQueen(r, c);
-    // 	    }
-    // 	    col++
+    // private int countQueens() {
+    // 	int total = 0;
+    // 	for (int r = 0; r < board.length; r++) {
+    // 	    for (int c = 0; c < board.length; c++) {
+    // 		if (board[r][c] == -1) {
+    // 		    total++;
     // 		}
+    // 	    }
+    // 	}
+    // 	return total;
+    // }
 
 
     public boolean solve() {
-	return solveHelper(0);
+	solveHelper(0);
+	board = realBoard;
+	return works;
+
     }
 
 
     
-    public boolean solveHelper(int c) {
-	if (countQueens() == board.length) {
-	    return true;
-	}
-	if (row == board.length) {
-	    return false;
+    private boolean solveHelper(int c) {
+	
+	boolean solExist = false;
+	 if (c >= board.length) {
+	     return true;
 	}
 	for (int r = 0; r < board.length; r++) {
+	    // if (board[r][c] == -1) {
+	    // 	return solveHelper( c + 1);
+	    // }
+	    // System.out.println(c);
+	    // System.out.println(r);
+	    // System.out.println('\n');
+	    
 
 	    if (board[r][c] == 0) {
 		addQueen(r,c);
-		// for (int row = 0; row < board.length; row ++) {
-		//     if (board[row][c] == -1 && row != r) {
-		//     removeQueen[r][c];
-		//     }
-		//     if (board [r][row] == -1 && row != c) {
-		//     removeQueen[r][c];
-		//     }
-		// }
-		//checkDiagonals();
-		if (!Helper(col + 1)) {
-		    removeQueen[r][c];
+		boolean solution = solveHelper(c + 1);
+		if (solution) {
+		    solExist = true;
+		    works = true;
+		    if( c == board.length - 1) {
+			realBoard = board.clone();
+			solutionCount += 1;
+		    }
 		}
-		if (board[r][c] == -1){
-		    setThreats();
-		    return Helper(col + 1);
-		}
+	       removeQueen(r,c);		    
 	    }
 	}
-	return false;
+	return solExist;
     }
 
-    public int getSolutionCount(){
-    	return -1;
+
+
+    public int getSolutionCount() {
+	solutionCount = 0;
+	if (solve()) {
+	    return solutionCount;
+	}
+	return -1;
     }
+
 
     public String toString(){
-    	return "";
+	String out = "";
+    	for (int r = 0; r < board.length; r ++) {
+	    out += "\n";
+	    for (int c = 0; c < board.length; c++) {
+	        out += board[r][c] + " ";
+	    }
+	}
+	return out;
+    }
+
+
+    public static void main(String[] args) {
+        QueenBoard test = new QueenBoard(8);
+	System.out.println(test.solve());
+	System.out.println(test);
+	System.out.println();
+	System.out.println(test.getSolutionCount());
+	// QueenBoard test2 = new QueenBoard(3);
+	// System.out.println( test2.solve());
+	// System.out.println(test2);
     }
 }
