0b38c2521b3433d10910b02235838fb2e95f06dc
JFreud <jrm.frdnbrg@gmail.com>
2017-02-26 19:57:14 -0500

    finished knightboard, not optimized

--- 03KnighTour/KnightBoard.java
+++ 03KnighTour/KnightBoard.java
@@ -1,64 +1,97 @@
+import java.util.ArrayList;
+
 public class KnightBoard {
     private int[][] board;
     private int sizeR, sizeC;
+    private int[][] moves = {{2,1}, {2,-1}, {1,2}, {1,-2}, {-2,1}, {-2,-1}, {-1,2}, {-1,-2}};
+    
 
+    
     public KnightBoard(int startingRows, int startingCols) {
 	board = new int[startingRows][startingCols];
 	sizeR = startingRows;
 	sizeC = startingCols;
     }
     
-    private boolean checkOpenFields(int r, int c) {
-	int[][] moves = new int{{2,1}, {2,-1}, {1,2}, {1,-2}, {-2,1}, {-2,-1}, {-1,2}, {-1,-2}};
+    private ArrayList<int[]> getOpenFields(int r, int c) {
+	ArrayList<int[]> validMoves = new ArrayList<int[]>();
 	for (int m = 0; m < moves.length; m++) {
-	    try {
-		board[r + moves[m][0]][c + moves[m][1]];
-		return true;
+	    if (!(r + moves[m][0] < 0 || c + moves[m][1] < 0 || r + moves[m][0] >= sizeR || c + moves[m][1] >= sizeC)) {
+		validMoves.add(moves[m]);
 	    }
-	    catch (ArrayIndexOutOfBoundsException e) {}
 	}
-	return false
-	    }
+	return validMoves;
+    }
+
 
      public void solve() {
-	 solveH(0,0,0);
+	 if (!(solveH(0,0,1))) {
+	     System.out.println("hi");
+	     board = new int[sizeR][sizeC];
+	 }
      }
 
      private boolean solveH (int row, int col, int level) {
-	 if (level == sizeR * sizeC) {
+	 if (level > sizeR * sizeC) {
 	    return true;
 	}
-	 for (int r = 0; r < board.length; r++) {
-	     for (int c =0; c < board[r].length; c++) {
-		 if (board[row][col] == 0 && checkOpenFields(row, col)) {
+	
+	 ArrayList<int[]> validMoves = getOpenFields(row, col);
+	 if (board[row][col] == 0 && getOpenFields(row, col).size() > 0) {
 		     board[row][col] = level;
-		     if (solveH (r, c, level++)) {
-			 return true;
+		     for (int m = 0; m < validMoves.size(); m++) {
+			
+			 level++;
+			
+			 if (solveH (row + validMoves.get(m)[0], col + validMoves.get(m)[1], level)) {
+			     return true;
+			 }
+			 level--;
 		     }
 		     board[row][col] = 0;
-		 }
-	     }
 	 }
+	 //System.out.println(level);
 	 return false;
      }
 		 
 	
-
-    public String toString() {
+    private static String arrayToString(ArrayList<int[]> ary) {
 	String out = "";
-	for (int r = 0; r < board.length; r++) {
+	for (int i = 0; i < ary.size(); i++) {
 	    out += "\n";
-	    for (int c = 0; c < board[r].length; c++) {
-		out += board[r][c];
+	    for (int p = 0; p < ary.get(i).length; p++) {
+		
+		out += ary.get(i)[p];
 	    }
 	}
 	return out;
     }
 
+	
+    public String toString() {
+	String out = "";
+	for (int r = 0; r < board.length; r++) {
+	    out += "\n";
+	    for (int c = 0; c < board[r].length; c++) {
+		//if (board[r][c] == 0) {
+		//    out += sizeR * sizeC + " ";
+		//}
+		 if (board[r][c] < 10) {
+		    out += " " + board[r][c] + " ";
+		    }
+		else{
+		    out += board[r][c] + " ";}
+	    }
+	}
+	return out;
+    }
 
     public static void main(String[] args) {
-	KnightBoard test = new KnightBoard(1,1);
+	KnightBoard test = new KnightBoard(4,3);
 	test.solve();
 	System.out.println(test);
+	KnightBoard test2 = new KnightBoard(6,6);
+	test2.solve();
+	System.out.println(test2);
     }
 }
