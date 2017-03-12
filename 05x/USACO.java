03c2dce0ad2194a36e9ef6dcf39c51dfc29e90c0
JFreud <jrm.frdnbrg@gmail.com>
2017-03-12 19:12:12 -0400

    does not work for 2 digit start/end yet

--- 05x/USACO.java
+++ 05x/USACO.java
@@ -2,16 +2,26 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class USACO {
 	private int[][] lake;
 	private int elevation;
 	private int numSteps;
 	private int[][] steps;
+
+    private char[][] pasture;
+    private int N;
+    private int M;
+    private int T;
+    private int[] start;
+    private int[] end;
+    private int[][] ary1;
+    private int[][] ary2;
 	
     public USACO(){}
     
-	public makelake(String filename){
+	public void makelake(String filename){
     	ArrayList<Integer> stuffs=new ArrayList<Integer>();
     	File infile=new File(filename);
     	Scanner inf ;
@@ -117,8 +127,135 @@ public class USACO {
 	return sum*72*72;
     }
 
+    public int bronze (String fileName) {
+	makelake(fileName);
+	return stomp();
+    }
+
+
+    
+    public void cowTravel(String fileName) {
+	ArrayList<Integer> inputNums = new ArrayList<Integer>();
+	ArrayList<String> field = new ArrayList<String>();
+	File file = new File(fileName);
+	Scanner sc;
+	try {
+	    sc = new Scanner(file);
+	    while (sc.hasNextInt()) {
+		inputNums.add(sc.nextInt());
+	    }
+	    sc.useDelimiter("");
+	    while (sc.hasNext()) {
+		field.add(sc.next());
+	    }
+
+	    int toRemove = field.size() - 8;
+
+	    inputNums.add(Integer.parseInt(field.remove(toRemove)));
+	     field.remove(toRemove);
+	     inputNums.add(Integer.parseInt(field.remove(toRemove)));
+	     field.remove(toRemove);
+	     inputNums.add(Integer.parseInt(field.remove(toRemove)));
+	     field.remove(toRemove);
+	     inputNums.add(Integer.parseInt(field.remove(toRemove)));
+
+	     while (field.remove("\n")) {};
+	     
+	    N = inputNums.remove(0);
+	    M = inputNums.remove(0);
+	    pasture = new char[N][M];
+	    T = inputNums.remove(0);
+	    start = new int[2];
+	    end = new int[2];
+	    start[0] = inputNums.remove(0);
+	    start[1] = inputNums.remove(0);
+	    end[0] = inputNums.remove(0);
+	    end[1] = inputNums.remove(0);
+
+	    ary1 = new int[N][M];
+	    ary2 = new int[N][M];
+	    for (int r = 0; r < pasture.length; r++) {
+		for (int c = 0; c < pasture[r].length; c++) {
+		    pasture[r][c] = field.remove(0).charAt(0);
+		    if (pasture[r][c] == '*') {
+			ary1[r][c] = -1;
+			ary2[r][c] = -1;
+		    }
+		    else {
+			ary1[r][c] = 0;
+			ary2[r][c] = 0;
+		    }
+		}
+	    }
+	    ary1[start[0] - 1][start[1] - 1] = 1;
+	    
+	}
+	catch (FileNotFoundException e) {
+	    System.out.println("Error 404: File Not Found");
+	    System.exit(0);
+	}
+    }
+    
+    public void updateArys() {
+	for (int r = 0; r < N; r++) {
+	    for (int c = 0; c < M; c++) {
+		if (ary1[r][c] > 0) {
+		    ary2[r][c] = 0;
+		    if (r + 1 >= 0 && r + 1 < N && ary2[r + 1][c] != -1)
+		    ary2[r+1][c] += ary1[r][c];
+		    if (r - 1 >= 0 && r - 1 < N && ary2[r - 1][c] != -1)
+		    ary2[r-1][c] += ary1[r][c];
+		    if (c + 1 >= 0 && c + 1 < M && ary2[r][c + 1] != -1)
+		    ary2[r][c+1] += ary1[r][c];
+		    if (c - 1 >= 0 && c - 1 < M && ary2[r][c - 1] != -1)
+		    ary2[r][c-1] += ary1[r][c];
+		}
+	    }
+	}
+
+
+	for (int r = 0; r < N; r++) {
+	    for (int c = 0; c < M; c++) {
+		ary1[r][c] = ary2[r][c];
+	    }
+	}
+	
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < M; c++) {
+		    if (pasture[r][c] == '*') {
+			ary2[r][c] = -1;
+		    }
+		    else {
+			ary2[r][c] = 0;
+		    }
+		}
+	}
+    }
+
+    public int silver(String fileName) {
+	cowTravel(fileName);
+	return silverH(end[0] - 1, end[1] - 1, T);
+    }
+
+    public int silverH(int eRow, int eCol, int time) {
+	if (time == 0) {
+	    return ary1[eRow][eCol];
+	}
+	updateArys();
+	// for (int r = 0; r < N; r++) {
+	//     for (int c = 0; c < M; c++) {
+	// 	System.out.print(ary1[r][c]);
+	//     }
+	//     System.out.println();
+	// }
+	// System.out.println("\n\n");
+	return silverH(eRow, eCol, time - 1);
+    }
+	    
+
 	public static void main(String[] args){
-		makelake margaret= new makelake("lake1.txt");
-		System.out.println(margaret.stomp());
+		USACO x= new USACO();
+		System.out.println (x.bronze("lake1.txt"));
+	        System.out.println(x.silver("ctravel.3.in"));
 	}
 }
