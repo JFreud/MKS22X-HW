0d2ca0631f7bd36be3d68f61ed8a641e0741185a
JFreud <jrm.frdnbrg@gmail.com>
2017-03-14 18:11:31 -0400

    tried partition

--- 07/Pivot.java
+++ 07/Pivot.java
@@ -2,22 +2,41 @@ import java.util.Random;
 
 public class Pivot {
     
-    public static int pivot(int[] ary) {
+    public static int partition(int[] ary) {
 	Random thing = new Random();
 	int pivot = thing.nextInt(ary.length);
-	partition(ary, pivot);
+	return partitionH(ary, pivot);
     }
 
-    public static partition(int[] ary, int pivot) {
+    public static int partitionH(int[] ary, int pivot) {
 	int temp;
 	int pivotVal = ary[pivot];
-	for (int i = 0; i < ary.length(); i++) {
-		if (ary[i] > pivotVal){
-		    temp = ary[i];
-		    ary[i] = ary[i+1];
-		    ary[i+1] = temp;
-		}
+	
+	swap(ary, pivot, ary.length-1);
+
+	int toSwap = 0;
+	for (int i = 0; i < ary.length; i++) {
+	    if (ary[i] < pivotVal) {
+		swap(ary, i, toSwap);
+		toSwap++;
+	    }
 	}
+
+	swap(ary, toSwap, ary.length-1);
+
+	return toSwap;
+    }
+
+    public static void swap(int[] ary, int indexA, int indexB) {
+	int temp;
+	temp = ary[indexA];
+	ary[indexA] = ary[indexB];
+	ary[indexB] = temp;
+    }
+
+    public static void main(String[] args) {
+	int[] test = {5,6,7,1,4,6};
+	System.out.println(partition(test));
     }
 
 }
