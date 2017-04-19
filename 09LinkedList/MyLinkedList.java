51c7e46f265c54186b90edbdba743943ceb1c7e9
JFreud <jrm.frdnbrg@gmail.com>
2017-04-19 19:19:38 -0400

    fixed bugs

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
+public class MyLinkedList implements Iterable<Integer> {
     private LNode start;
     private LNode end;
     private int size;
@@ -12,11 +12,11 @@ public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
     	size = 0;
     }
 
-    public Iterator<LNode> iterator() {
+    public Iterator<Integer> iterator() {
 	return new LinkedListerator();
     }
 
-    private class LinkedListerator implements Iterator<LNode> {
+    private class LinkedListerator implements Iterator<Integer> {
 	LNode nextL;
 
 	public LinkedListerator() {
@@ -30,11 +30,11 @@ public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
 	    return false;
 	}
 
-	public LNode next() {
+	public Integer next() {
 	    if (nextL != null) {
 		LNode toRet = nextL;
 		nextL = nextL.next;
-		return toRet;
+		return toRet.hereNum;
 	    }
 	    throw new NoSuchElementException();
 	}
@@ -144,8 +144,12 @@ public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
     }
 
     public void add(int index, int value) {
-	if (index >= size || index < 0) {
+	if (index > size || index < 0) {
 	    throw new IndexOutOfBoundsException();
+	}
+	if (index == size) {
+	    add(value);
+	    return;
 	}
 	LNode toAdd = start;
 	for (int i = 0; i < index - 1; i++) {
@@ -165,8 +169,8 @@ public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
 	}
 	if (index == 0) {
 	    retVal = start.hereNum;
-	    start = start.next;
 	    start.prev = null;
+	    start = start.next;
 	    size--;
 	    return retVal;
 	}
@@ -249,11 +253,63 @@ public class MyLinkedList implements Iterable<MyLinkedList.LNode> {
 	//     System.out.println(l.hereNum);
 	// }
 
-	System.out.println(test);
+	//System.out.println(test);
 	// System.out.println(test.indexOf(11));
+	MyLinkedList a= new MyLinkedList();
+	System.out.println(a.toString()+"\nSize: "+a.size());//[], size=0
+	for(int i=0; i<20; i++){
+	    a.add(i);
+	    if(a.get(a.size()-1)%2==0){
+		//System.out.println(a.size());
+		a.remove(a.size()-1);
+	    }
+	    //System.out.println(a);//Check if all nums arent odd
+	}
 	
+	System.out.println(a+"\nSize: "+a.size());//odds 0-20 in order, size=10
+	
+	//Testing: set, indexOf, add(i,v); reinforce others
+	for(int i=0; i<10; i++){
+	    a.set(i,i*100);
+	}
+	
+	System.out.println(a+"\nSize: "+a.size());//mulitples of 100, size=10
+	for(int i=0; i<10; i++){
+	    a.set(i,a.indexOf(i*100));
+	}
+	
+        System.out.println(a+"\nSize: "+a.size());//0-9 in order, size=10
+	a.remove(1);
+	a.add(1,1);
+	for(int i=0; i<10; i++){
+	    a.add(0,i*-1-1);
+	}
+	a.add(a.size(),10);
+	
+	System.out.println(a+"\nSize: "+a.size());//-10-10 in order, size=21
+	
+	//Testing: Iterator
+	MyLinkedList data = new MyLinkedList();
+	int i = 0;
+	while(i < 15){
+	    data.add(i);
+	    i++;
+	}
+
+	System.out.println("\nContents: "+data);
+	System.out.println("Standard loop:");
+
+	for(int n = 0; n < data.size(); n++){
+	    System.out.print(data.get(n)+" ");
+	}
+	System.out.println();
+	System.out.println("for-each loop:");
+	for(Integer s : data){
+	    System.out.print(s+" ");
+	}
+	//should be same
     }
-	    
+	
 }
 
 
