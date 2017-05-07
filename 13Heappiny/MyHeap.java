1246ab6977a996effae18600aadc38bd3c0881d3
JFreud <jrm.frdnbrg@gmail.com>
2017-05-07 13:25:34 -0400

    max heap works

--- 13Heappiny/MyHeap.java
+++ 13Heappiny/MyHeap.java
@@ -6,59 +6,72 @@ public class MyHeap {
 
     public MyHeap() {
 	heapThing = new ArrayList<String>();
-	heapThing.add(""); //temp
+	heapThing.add("");
     }
 
     public MyHeap(boolean ifMax) {
 	isMax = ifMax;
 	heapThing = new ArrayList<String>();
-	if (isMax){
-	    heapThing.add("");
-	}
-	else {
-	    heapThing.add("~~~~~~~~~"); //temp
-	}
+        heapThing.add("");
     }
 
     private void pushUp(int index) {
-	String temp = heapThing.get(index / 2);
-	String toUp = heapThing.get(index);
-	heapThing.set(index / 2, toUp);
-	heapThing.set(index, temp);
+	String temp;
+	String value = heapThing.get(index);
+	while (index != 1 && value.compareTo(heapThing.get(index / 2)) > 0) {
+	    temp = heapThing.get(index / 2);
+	    heapThing.set(index / 2, heapThing.get(index));
+	    heapThing.set(index, temp);
+	    index = index / 2;
+	}	
     }
 
     private void pushDown(int index) {
-	if (heapThing.get(index * 2).compareTo(heapThing.get(index * 2 + 1)) < 0) {
-	    index = index * 2 + 1;
+	String value = heapThing.get(index);
+	String temp;
+	int newIndex;
+        while (!(index > (heapThing.size() - 1) / 2) && (value.compareTo(heapThing.get(index * 2)) < 0 || value.compareTo(heapThing.get(index * 2 + 1)) < 0)){
+	    String child1 = heapThing.get(index * 2);
+	    String child2 = heapThing.get(index * 2 + 1);
+	    if (child1.compareTo(child2) <= 0){ 
+		temp = child2;
+		heapThing.set(index * 2 + 1, value);
+		newIndex = index * 2 + 1;
+	    }
+	    else {
+		temp = child1;
+		heapThing.set(index * 2, value);
+		newIndex = index * 2;
+	    }
+	    heapThing.set(index, temp);
+	    index = newIndex;
 	}
-	else {
-	    index = index * 2;
-	}
-        pushUp(index);
     }
 
     public void add(String s) {
 	heapThing.add(s);
-	if (heapThing.size() == 2) {
-	    return;
-	}
-	if (isMax) {
-	    while (s.compareTo(heapThing.get(heapThing.lastIndexOf(s) / 2)) <= 0) {
-		pushUp(heapThing.lastIndexOf(s));
-	    }
-	} else {
-	    while (s.compareTo(heapThing.get(heapThing.lastIndexOf(s) / 2)) > 0) {
-		pushUp(heapThing.lastIndexOf(s));
-	    }
+	if (heapThing.size() > 2) {
+	    pushUp(heapThing.size() - 1);
 	}
     }
 
     public String remove() {
-	return heapThing.remove(heapThing.size() - 1);
+    	String toRet = heapThing.get(1);
+    	if (heapThing.size() - 1 == 1) {
+    	    return heapThing.remove(1);
+    	}
+    	String tempTop = heapThing.get(heapThing.size() - 1);
+    	heapThing.set(1, tempTop);
+    	heapThing.remove(heapThing.size() - 1);
+    	pushDown(1);
+	return toRet;
     }
+        
+        
+	    
 
     public String peek() {
-	return heapThing.get(heapThing.size() - 1);
+	return heapThing.get(1);
     }
 
     public String toString() {
@@ -75,6 +88,11 @@ public class MyHeap {
 	test.add("potatoa");
 	test.add("potatoaa");
 	System.out.println(test);
+	test.add("john");
+	System.out.println(test);
+	System.out.println(test.remove());
+	System.out.println(test);
+	System.out.println(test.peek());
 	
     }
 }
