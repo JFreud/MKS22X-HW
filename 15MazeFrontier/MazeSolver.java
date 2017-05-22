9ea38e05cc9609fddfa61095356e5561deea43c2
JFreud <jrm.frdnbrg@gmail.com>
2017-05-21 22:32:49 -0400

    finished MazeSolver yay

--- 15MazeFrontier/MazeSolver.java
+++ 15MazeFrontier/MazeSolver.java
@@ -17,18 +17,89 @@ public class MazeSolver {
 
     public void solve (int style) {
 	//style is 0-4, where 0-DFS, 1-BFS,2-BestFirst, 3-A*
+
+	Location current;
+	int crow, ccol;
+	int[] dir = {-1, 1};
+        Frontier structure;
+	boolean aStar = false;;
+
+	
 	if (style == 0) {
-	    solveDFS();
+	    structure = new FrontierStack();
 	}
-	if (style == 1) {
-	    solveBFS();
+	else if (style == 1) {
+	    structure = new FrontierQueue();
 	}
-	if (style == 2) {
-	    solvePQ(false);
+	else if (style == 2) {
+	    structure = new FrontierPriorityQueue();
 	}
-	else {
-	    solvePQ(true);
+	else if (style == 3) {
+	    structure = new FrontierPriorityQueue();
+	    aStar = true;
 	}
+	else{
+	    structure = new FrontierPriorityQueue();
+	    System.out.println("Invalid Input, use 0,1,2,3, using Best-First");
+	}
+
+	structure.add(maze.getStart());
+
+	while (structure != null) {
+	    System.out.println(maze.toString(100));
+	    current = structure.next();
+	    crow = current.getRow();
+	    ccol = current.getCol();
+
+	    if (current.getGoalDist() == 0) {
+		// System.out.println(maze.getEnd().getGoalDist());
+		// System.out.println(current.getGoalDist());
+		// System.out.println(crow);
+		// System.out.println(ccol);
+		// System.out.println(maze.getEnd().getRow());
+		// System.out.println(maze.getEnd().getRow());
+		System.out.println("end");
+		traceBack(current);
+		return;
+	    }
+	    else {
+		maze.set(crow, ccol, '@');
+	    }
+		
+	    
+	    for (int i = 0; i < dir.length; i++) {
+		//maybe check if out of bounds
+
+		// System.out.println(maze.getEnd().getRow() - crow +
+		// 		   maze.getEnd().getCol() - (ccol + dir[i]));
+		
+		int startDistR = Math.abs(maze.getStart().getRow() - (crow + dir[i])) +
+		    Math.abs(maze.getStart().getCol() - ccol);
+		int endDistR = Math.abs(maze.getEnd().getRow() - (crow + dir[i])) +
+		    Math.abs(maze.getEnd().getCol() - ccol);
+		int startDistC = Math.abs(maze.getStart().getRow() - crow) +
+		    Math.abs(maze.getStart().getCol() - (ccol + dir[i]));
+		int endDistC = Math.abs(maze.getEnd().getRow() - crow) +
+		    Math.abs(maze.getEnd().getCol() - (ccol + dir[i]));
+		
+		if (maze.get(crow + dir[i], ccol) != '#' &&
+		    maze.get(crow + dir[i], ccol) != '@' &&
+		    maze.get(crow + dir[i], ccol) != '.') {
+		    structure.add(new Location(crow + dir[i], ccol, current, startDistR, endDistR, aStar));
+		    maze.set(crow + dir[i], ccol, '?');
+		}
+		if (maze.get(crow, ccol + dir[i]) != '#' &&
+			 maze.get(crow, ccol + dir[i]) != '@' &&
+			 maze.get(crow, ccol + dir[i]) != '.') {
+		    structure.add(new Location(crow, ccol + dir[i], current, startDistC, endDistC, aStar));
+		    maze.set(crow, ccol + dir[i], '?');
+		}
+	    }
+
+	    maze.set(crow, ccol, '.');
+	}
+
+	
     }
 
     //trace back function
@@ -40,180 +111,32 @@ public class MazeSolver {
 	ccol = loc.getCol();
 	maze.set(crow, ccol, 'E');
         cur = loc.getPrev();
-	while (cur.compareTo(maze.getStart()) != 0){
+	while (cur.getStartDist() != 0){
+            // System.out.println(crow);
+	    // System.out.println(ccol);
+	    //System.out.println("tracing");
+	    System.out.println(maze.toString(100));
+	    //System.out.println(cur.getStartDist());
 	    crow = cur.getRow();
 	    ccol = cur.getCol();
 	    maze.set(crow, ccol, '@');
 	    cur = cur.getPrev();
 	}
-	maze.set(crow, ccol, 'S');
+	maze.set(cur.getRow(), cur.getCol(), 'S');
+	System.out.println(maze.toString(200));
     }
 	
-    //this is really repetetive.... fix
 
+    public String toString() {
+	return maze.toString();
+    }
     
-    private void solveDFS() {
-	Location current;
-	int crow, ccol;
-	FrontierStack fStack = new FrontierStack();
-	int[] dir = {-1, 1};
-
-	
-        fStack.add(maze.getStart());
-
-	while (fStack != null) {
-	    current = fStack.next();
-	    crow = current.getRow();
-	    ccol = current.getCol();
-
-	    if (current.compareTo(maze.getEnd()) == 0) {
-		traceBack(current);
-		return;
-	    }
-	    else {
-		maze.set(crow, ccol, '@');
-	    }
-		
-	    
-	    for (int i = 0; i < dir.length; i++) {
-		//maybe check if out of bounds
-
-		int startDistR = maze.getStart().getRow() - (crow + dir[i]) +
-		    maze.getStart().getCol() - ccol;
-		int endDistR = maze.getEnd().getRow() - (crow + dir[i]) +
-		    maze.getEnd().getCol() - ccol;
-		int startDistC = maze.getStart().getRow() - crow +
-		    maze.getStart().getCol() - (ccol + dir[i]);
-		int endDistC = maze.getEnd().getRow() - crow +
-		    maze.getEnd().getCol() - (ccol + dir[i]);
-		
-		if (maze.get(crow + dir[i], ccol) != '#' &&
-		    maze.get(crow + dir[i], ccol) != '@') {
-		    fStack.add(new Location(crow + dir[i], ccol, current, startDistR, endDistR, false));
-		    maze.set(crow + dir[i], ccol, '?');
-		}
-		else if (maze.get(crow, ccol + dir[i]) != '#' &&
-			 maze.get(crow, ccol + dir[i]) != '@') {
-		    fStack.add(new Location(crow, ccol + dir[i], current, startDistC, endDistC, false));
-		    maze.set(crow, ccol + dir[i], '?');
-		}
-	    }
-
-	    maze.set(crow, ccol, '.');
-	}
-	
-
-    }
-
-
-    private void solveBFS() {
-	Location current;
-	int crow, ccol;
-	FrontierQueue fQueue = new FrontierQueue();
-	int[] dir = {-1, 1};
-
-	fQueue.add(maze.getStart());
-
-	while (fQueue != null) {
-	    current = fQueue.next();
-	    crow = current.getRow();
-	    ccol = current.getCol();
-
-	    if (current.compareTo(maze.getEnd()) == 0) {
-		traceBack(current);
-		return;
-	    }
-	    else {
-		maze.set(crow, ccol, '@');
-	    }
-		
-	    
-	    for (int i = 0; i < dir.length; i++) {
-		//maybe check if out of bounds
-		int startDistR = maze.getStart().getRow() - (crow + dir[i]) +
-		    maze.getStart().getCol() - ccol;
-		int endDistR = maze.getEnd().getRow() - (crow + dir[i]) +
-		    maze.getEnd().getCol() - ccol;
-		int startDistC = maze.getStart().getRow() - crow +
-		    maze.getStart().getCol() - (ccol + dir[i]);
-		int endDistC = maze.getEnd().getRow() - crow +
-		    maze.getEnd().getCol() - (ccol + dir[i]);
-
-		
-		if (maze.get(crow + dir[i], ccol) != '#' &&
-		    maze.get(crow + dir[i], ccol) != '@') {	    
-		    fQueue.add(new Location(crow + dir[i], ccol, current, startDistR, endDistR, false));
-		    maze.set(crow + dir[i], ccol, '?');
-		}
-		else if (maze.get(crow, ccol + dir[i]) != '#' &&
-			 maze.get(crow, ccol + dir[i]) != '@') {
-		    fQueue.add(new Location(crow, ccol + dir[i], current, startDistC, endDistC, false));
-		    maze.set(crow, ccol + dir[i], '?');
-		}
-	    }
-
-	    maze.set(crow, ccol, '.');
-	}
-	
-    }
-
-    private void solvePQ(boolean aStar) {
-	Location current;
-	int crow, ccol;
-	FrontierPriorityQueue fPQueue = new FrontierPriorityQueue();
-	int[] dir = {-1, 1};
-
-	fPQueue.add(maze.getStart());
-	
-	while (fPQueue != null) {
-	    current = fPQueue.next();
-	    crow = current.getRow();
-	    ccol = current.getCol();
-
-	    if (current.compareTo(maze.getEnd()) == 0) {
-		traceBack(current);
-		return;
-	    }
-	    else {
-		maze.set(crow, ccol, '@');
-	    }
-
-
-	    for (int i = 0; i < dir.length; i++) {
-		//maybe check if out of bounds
-
-		int startDistR = maze.getStart().getRow() - (crow + dir[i]) +
-		    maze.getStart().getCol() - ccol;
-		int endDistR = maze.getEnd().getRow() - (crow + dir[i]) +
-		    maze.getEnd().getCol() - ccol;
-		int startDistC = maze.getStart().getRow() - crow +
-		    maze.getStart().getCol() - (ccol + dir[i]);
-		int endDistC = maze.getEnd().getRow() - crow +
-		    maze.getEnd().getCol() - (ccol + dir[i]);
-
-		
-		if (maze.get(crow + dir[i], ccol) != '#' &&
-		    maze.get(crow + dir[i], ccol) != '@') {    
-		    fPQueue.add(new Location(crow + dir[i], ccol, current, startDistR, endDistR, aStar));
-		    maze.set(crow + dir[i], ccol, '?');
- 
-		}
-		else if (maze.get(crow, ccol + dir[i]) != '#' &&
-			 maze.get(crow, ccol + dir[i]) != '@') {
-		    fPQueue.add(new Location(crow, ccol + dir[i], current, startDistC, endDistC, aStar));
-		    maze.set(crow, ccol + dir[i], '?');
-		}
-	    }
-
-	    maze.set(crow, ccol, '.');
-
-	}
-    }
 
 
     public static void main(String[] args) {
-	MazeSolver test = new MazeSolver("data1.dat");
-	test.solve(0);
+	MazeSolver test = new MazeSolver("data3.dat");
+	test.solve(1);
+	//System.out.println(test);
     }
 
 
